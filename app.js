5203ab90ec8737e93595e8488b087d1500229a5e
Gian Zamora <gz2016@att.net>
2021-05-04 11:55:30 -0700

    updated functionality for the view all employees by manager function

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const connectionDB = require('./config/connection.js');
 let databaseConnect = mysql.createConnection(connectionDB);
 
 function startApp () {
-    databaseConnect.connect(function (err) {
-        if (err) throw err;
-    });
     console.log('Welcome to the Employee Database!');
+    // this reconnects to the DB
+    connectToDB();
+    // This begins the inquirer prompt
     startMenu();
 };
+
+function connectToDB() {
+    databaseConnect.connect(function (err) {
+        if (err) throw err;
+        console.log('Connected to database...');
+    });
+}
 
 function startMenu() {
     inquirer.prompt(
@@ -36,8 +43,11 @@ function startMenu() {
             updateEmployee();
         } else if (answer.restart == 'Update Employee Manager') {
             updateEmployeeM();
-        }
-    })
+        } else if (answer.restart == 'Exit') {
+            // this disconnects the program from the DB
+            databaseConnect.end();
+        } 
+    });
 };
 
 
@@ -61,15 +71,36 @@ function viewEmployees(employee) {
 
         // Console log whole employee list
         console.log('Here are all active Employees:');
-        console.table(res);
+        console.table(eArray);
+        startMenu();
 
-        // end the connection to employee_db
-        databaseConnect.end();
-    });
+    });    
 };
 
 function viewEmployeesBM() {
-    
+    let newQuery = "SELECT CONCAT(b.first_name, ' ', b.last_name) AS manager, a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary FROM employee as a LEFT JOIN role on a.role_id = role.id LEFT JOIN department on role.department_id = department.id INNER JOIN employee as b on b.id = a.manager_id ORDER BY manager ASC ;";
+    let eArray = [];
+
+    databaseConnect.query(newQuery, (err, res) => {
+        if (err) throw err;
+        res.forEach((employee) => {
+            eArray.push({
+                'id': employee.id, 
+                'first_name': employee.first_name,
+                'last_name': employee.last_name, 
+                'title': employee.title,
+                'department': employee.department,
+                'salary': employee.salary,
+                'manager': employee.manager,
+            });
+        });
+
+        // Console table whole employee list by manager
+        console.log('Here are all active Employees organized by Manager:');
+        console.table(eArray);
+        startMenu();
+
+    });
 };
 
 function viewEmployeesBD() {
