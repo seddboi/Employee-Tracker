fded7a93b32e400a1af9931d5074b8fc9204162a
Gian Zamora <gz2016@att.net>
2021-05-04 14:19:40 -0700

    added view employees by department functionality... still working on other functionalities

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function startMenu() {
 
 function viewEmployees(employee) {
     let newQuery = "SELECT a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary, CONCAT(b.first_name, ' ', b.last_name) AS manager FROM employee as a LEFT JOIN role on a.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee as b on b.id = a.manager_id;";
+    // creation of array allows for reorganization of columns to my liking :)
     let eArray = [];
 
     databaseConnect.query(newQuery, (err, res) => {
@@ -78,37 +79,106 @@ function viewEmployees(employee) {
 };
 
 function viewEmployeesBM() {
-    let newQuery = "SELECT CONCAT(b.first_name, ' ', b.last_name) AS manager, a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary FROM employee as a LEFT JOIN role on a.role_id = role.id LEFT JOIN department on role.department_id = department.id INNER JOIN employee as b on b.id = a.manager_id ORDER BY manager ASC ;";
+    let newQuery = "SELECT a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary, CONCAT(b.first_name, ' ', b.last_name) AS manager FROM employee as a LEFT JOIN role on a.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee as b on b.id = a.manager_id ORDER BY manager ASC;";
     let eArray = [];
 
     databaseConnect.query(newQuery, (err, res) => {
         if (err) throw err;
         res.forEach((employee) => {
             eArray.push({
+                'manager': employee.manager,
                 'id': employee.id, 
                 'first_name': employee.first_name,
                 'last_name': employee.last_name, 
                 'title': employee.title,
                 'department': employee.department,
                 'salary': employee.salary,
-                'manager': employee.manager,
             });
         });
 
-        // Console table whole employee list by manager
+        // Console table whole llst by Manager
         console.log('Here are all active Employees organized by Manager:');
+        console.table(eArray);
+        startMenu();
+    });    
+};
+
+function viewEmployeesBD() {
+    let newQuery = "SELECT a.id, a.first_name, a.last_name, role.title, department.name AS department, role.salary, CONCAT(b.first_name, ' ', b.last_name) AS manager FROM employee as a LEFT JOIN role on a.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee as b on b.id = a.manager_id ORDER BY department ASC;";
+    let eArray = [];
+
+    databaseConnect.query(newQuery, (err, res) => {
+        if (err) throw err;
+        res.forEach((employee) => {
+            eArray.push({
+                'department': employee.department,
+                'id': employee.id, 
+                'first_name': employee.first_name,
+                'last_name': employee.last_name, 
+                'title': employee.title,
+                'salary': employee.salary,
+                'manager': employee.manager,
+            });
+        });
+        
+        // Console table whole employee list by department
+        console.log('Here are all active Employees organized by Department:');
         console.table(eArray);
         startMenu();
 
     });
 };
 
-function viewEmployeesBD() {
-    
-};
-
 function addEmployee() {
+//     inquirer.prompt([
+//         {
+//             name: 'first_name', 
+//             type: 'input', 
+//             message: 'What is the new employee\'s first name?'
+//         },
+//         {
+//             name: 'last_name', 
+//             type: 'input', 
+//             message: 'What is the new employee\'s last name?'
+//         },
+//         {
+//             name: 'role', 
+//             type: 'list', 
+//             message: 'What is the employee\'s role?',
+//             choices: availableRoles
+//         },
+//         {
+//             name: 'manager', 
+//             type: 'list', 
+//             message: 'What is the employee\'s role?',
+//             choices: availableEmployees
+//         }
+//     ]);
+
+    function getRoles() {
+        let roles = [];
+
+        databaseConnect.query('SELECT * FROM role;', (err, res) => {
+            res.forEach((role) => {
+                roles.push(
+                    {
+                        'title': role.title,
+                        'id': role.id
+                    }
+                );
+            });
+        });
+        console.log(roles);
+    };
+
+    getRoles();
     
+    // let getRoles = (roles) => {
+
+    //     roles.forEach((x) => {
+    //         rolesArray.push(x.title);
+    //     });
+    // };
 };
 
 function removeEmployee() {
