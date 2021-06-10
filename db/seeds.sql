6bc850dbbea979162564b1d002ec65f22b3f0f20
Gian Zamora <gz2016@att.net>
2021-06-10 09:13:13 -0700

    updated seeds file to install separate id from role id

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 USE employees_db;
 
-INSERT INTO department(name) VALUES ('Frontline'), ('Marketing'), ('Corporate');
+INSERT INTO department (id, name) VALUES (1,'Frontline'), (2, 'Marketing'), (3, 'Corporate');
 
-INSERT INTO role(title, salary, department_id)
-VAlUES ('Manager', 60000, 1), ('Crew Member', 20000, 1), ('Shift Lead', 25000, 1), ('Marketing Director', 70000, 2), ('Trainer/Operations Director', 70000, 3), ('Operations Manager', 60000, 1);
+INSERT INTO role(id, title, salary, department_id)
+VAlUES (1, 'Manager', 60000, 1), (2, 'Crew Member', 20000, 1), (3, 'Shift Lead', 25000, 1), (4, 'Marketing Director', 70000, 2), (5, 'Trainer/Operations Director', 70000, 3), (6, 'Operations Manager', 65000, 1);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-VALUES ('Bill', 'Bobilly', 1, NULL), ('Chad', 'Chadson', 1, 1), ('Gary', 'Baker', 2, 1), ('Lisa', 'Lozenge', 3, NULL), ('Jerry', 'Richards', 4, NULL), ('Katie' , 'Bobatie', 5, NULL);
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id) 
+VALUES (3, 'Bill', 'Bobilly', 1, NULL), (5, 'Chad', 'Chadson', 1, 1), (1, 'Gary', 'Baker', 2, 1), (4, 'Lisa', 'Lozenge', 3, NULL), (2, 'Jerry', 'Richards', 4, NULL), (6, 'Katie' , 'Bobatie', 5, NULL);
