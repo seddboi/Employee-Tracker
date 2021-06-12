98dcd22b56625af559cebfa0aa773ac9f9da2b15
Gian Zamora <gz2016@att.net>
2021-06-11 23:06:30 -0700

    role to roles since role is a reserved word in mysql

--- db/schema.sql
+++ db/schema.sql
@@ -7,20 +7,25 @@ USE employees_db;
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY (id),
+    FOREIGN_key (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL, 
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY(id),
+    PRIMARY KEY (id),
+    FOREIGN_KEY (role_id) REFERENCES roles (id),
+    FOREIGN_KEY (manager_id) REFERENCES employee(id)
 );
